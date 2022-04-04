7079ed6a89c8de47f79292964f0693847bd4b192
nargamard <89466942+nargamard@users.noreply.github.com>
2022-04-04 22:33:41 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ terraform {
 }
 
 provider "yandex" {
+  token                    = "t1.9euelZqPyZDLzZqXzpuMlpWPyIyKye3rnpWazomSkMibkM-XkZOVmJaUyszl8_dWe1Jt-e9VeHJs_d3z9xYqUG3571V4cmz9.Cn0Cqf9f8DXk8xKpPV64pIhJbvvo5Bx89uaAUg6T3ELvJNz25QI0-ZY8rlAsGeFzhie9sMH3WCv2JBc5doggBg"
   cloud_id                 = var.yandex_cloud_id
   folder_id                = var.yandex_folder_id
   zone                     = var.zone
