5d4486c251fbeee38de9e23cb06ae66e2ff04e82
nargamard <89466942+nargamard@users.noreply.github.com>
2022-04-04 22:26:36 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,6 @@ terraform {
 }
 
 provider "yandex" {
-  service_account_key_file = var.token_path
   cloud_id                 = var.yandex_cloud_id
   folder_id                = var.yandex_folder_id
   zone                     = var.zone
